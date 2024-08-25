19a61b8f83690356f1546d6bfd5b4a0783413e6f
Ruth Prudence <ruth@ruthprudence.com>
2024-08-25 04:28:45 -0700

    added node_modules/ to .gitignore

--- scripts/backend/createFilmCard.js
+++ scripts/backend/createFilmCard.js
@@ -1,23 +1,31 @@
-/**
+import getMoviePoster from "./getMoviePoster.js";/**
  * Creates HTML for a film card.
  * @param {object} film - The film object to create HTML for.
  * @returns {string} HTML for the film card.
  */
-export default function createFilmCard(film) {
-  return `
-    <div class="film-card" onclick="this.classList.toggle('show-back')">
-      <div class="film-card-inner">
-        <div class="film-card-front">
-          <img src="" alt="${film.title}" width="180" height="280">
-        </div>
-        <div class="film-card-back">
-          <h3 class="card-title">${film.title}</h3>
-          <p class="release-year">(${film.released})</p>
-          <p class="seen">Seen</p>
-          <p class="date-watched">${film.dateWatched}</p>
-          <p class="opinion">${film.opinion}</p>
+const createFilmCard = async (film) => {
+  try {
+    const posterUrl = await getMoviePoster(film.title, film.year);
+    const filmCardHTML = `
+      <div class="film-card" onclick="this.classList.toggle('show-back')">
+        <div class="film-card-inner">
+          <div class="film-card-front">
+            <img src="${posterUrl}" alt="${film.title}" width="180" height="280">
+          </div>
+          <div class="film-card-back">
+            <h3 class="card-title">${film.title}</h3>
+            <p class="release-year">(${film.year})</p>
+            <p class="seen">Seen</p>
+            <p class="date-watched">${film.dateWatched}</p>
+            <p class="opinion">${film.opinion}</p>
+          </div>
         </div>
       </div>
-    </div>
-  `;
-}
+    `;
+    return filmCardHTML;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default createFilmCard;
---
+++ scripts/backend/getMoviePoster.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+  const tmdbApiKey = process.env.TMDB_API_KEY;
+  const tmdbApiUrl = 'https://api.themoviedb.org/3';
+    
+  const getMoviePoster = async (title, year) => {
+    try {
+      const response = await axios.get(`${tmdbApiUrl}/search/movie`, {
+        params: {
+          api_key: tmdbApiKey,
+          query: `${title} (${year})`,
+        },
+      });
+  
+      const results = response.data.results;
+      if (results.length > 0) {
+        const posterPath = results[0].poster_path;
+        const posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
+        return posterUrl;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  export default getMoviePoster;
