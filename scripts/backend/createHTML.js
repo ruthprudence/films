b351dab538cb187da32279befdb793e20872f566
Ruth Prudence <ruth@ruthprudence.com>
2024-09-03 10:07:17 -0700

    removed images for now, adjusted CSS

--- scripts/backend/createHTML.js
+++ scripts/backend/createHTML.js
@@ -10,14 +10,12 @@ const createHTML = (films) => {
       <div class="film-card" data-title="${film.title}" data-year="${film.released}" onclick="this.classList.toggle('show-back')">
         <div class="film-card-inner">
           <div class="film-card-front">
-            ${hasImage ? '' : `<span>${film.title}</span>`}
-            <img src="${film.imgSrc}" alt="${film.title}" width="180" height="280" style="${hasImage ? '' : 'display: none'}">
+            <span>${film.title}</span>
           </div>
           <div class="film-card-back">
-            <h3 class="card-title"><a href="https://en.wikipedia.org/wiki/${film.wikipediaLink}" style="text-decoration: none; color: inherit">${film.title}</a></h3>
+            <h3 class="card-title"><a href="https://en.wikipedia.org/wiki/${film.wikipediaLink}">${film.title}</a></h3>
             <p class="release-year">(${film.released})</p>
-            <p class="seen">Seen</p>
-            <p class="date-watched">${film.dateWatched}</p>
+            <p class="date-watched">Seen: ${film.dateWatched}</p>
             <p class="opinion">${film.opinion}</p>
           </div>
         </div>
